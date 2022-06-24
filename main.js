b8d2c296da2a0d95f782b12a28ee25ac5e358a8e
Skeletoos57 <89609124+Skeletoos57@users.noreply.github.com>
2022-06-24 14:40:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let thiagoButton = document.getElementById("thiagoButton");
+let diegoButton = document.getElementById("diegoButton");
+
+thiagoButton.addEventListener('click', () => {
+    window.open("https://open.spotify.com/show/6RfDcgnkvkDhrGwZqcYizv");
+});
+
+diegoButton.addEventListener('click', () => {
+    window.open("https://www.canva.com/design/DAFDDx8a924/j0BIpPJp3yPMU951UltYuA/watch?utm_content=DAFDDx8a924&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink");
+});
